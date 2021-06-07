ba48957c68ecb4d0d92afb63165e895651ea93e5
unknown <adrian.20191176@iit.ac.lk>
2021-06-07 23:57:37 +0530

    minor changes

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = config.get('mongoURI');
 
 const connectDB  = async () =>{
     try{
-        await mongoose.connect(db)
+        await mongoose.connect(db,{useNewUrlParser:true,  useUnifiedTopology: true })
         console.log('Database connected');
     }catch(err){
         console.error(err.message);
